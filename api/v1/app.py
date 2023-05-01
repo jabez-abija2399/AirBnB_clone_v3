3b114ac41c46af97d679509733488a6fb9c8d489
jabez kifle <111049968+jabez-abija2399@users.noreply.github.com>
2023-05-01 16:10:14 -0700

    file permsion changed into excutable file

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,4 @@ def teardown(exception):
 
 # Run Flask application
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", 5000)
-    app.run(host=host, port=port, threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
