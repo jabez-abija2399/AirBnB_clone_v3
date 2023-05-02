a5df39405a42ceebf5b0400fa5ef9afe1b93e30a
jabez kifle <111049968+jabez-abija2399@users.noreply.github.com>
2023-05-01 17:37:45 -0700

    Not found

--- api/v1/views/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Initialize the views module."""
-from flask import Blueprint
-
-# Create a Blueprint object
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Import views module to register the blueprints
-from api.v1.views.index import *
